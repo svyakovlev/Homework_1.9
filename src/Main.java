9bedd48bedf6f534b8951be1a828aa9605c2e583
Сергей <s.v.yakovlev@yandex.ru>
2022-07-01 00:24:41 +0400

    Homework is done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-
+        Author levTolstoy = new Author("Лев", "Толстой");
+        Author nikolayNosov = new Author("Николай", "Носов");
+        Book warAndPeace = new Book("Война и мир", levTolstoy, 1863);
+        Book dunnoOnTheMoon = new Book("Незнайка на Луне", nikolayNosov, 1964);
+        dunnoOnTheMoon.setYear(1965);
     }
 }
